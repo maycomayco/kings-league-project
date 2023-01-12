6990e9f642b3d61ee351211ba726a9f67c7d86a2
Mayco Barale <maycobarale@gmail.com>
2023-01-12 12:56:01 -0300

    add log utils for presidents scraper

--- scraping/presidents.js
+++ scraping/presidents.js
@@ -1,5 +1,6 @@
 import { writeFile, readFile } from "node:fs/promises";
 import path from "node:path";
+import { logInfo, logSuccess } from "./log";
 
 const STATICS_PATH = path.join(process.cwd(), "./assets/static/presidents");
 const DB_PATH = path.join(process.cwd(), "./db");
@@ -16,6 +17,8 @@ const presidents = await Promise.all(
     const { rendered: name } = title;
 
     const { "wp:attachment": attachment } = links;
+    logInfo(`> Fetching attachment for president: ${name}`);
+
     // image URL
     const { href: imageApiEndpoint } = attachment[0];
 
@@ -31,24 +34,27 @@ const presidents = await Promise.all(
     //get file extension
     const extension = imageUrl.split(".").at(-1);
 
-    console.log(`Fetching img for ${name}...`);
+    logInfo(`> Fetching image for president: ${name}`);
 
     // download image file with buffer
     const responseImage = await fetch(imageUrl);
     const arrayBuffer = await responseImage.arrayBuffer();
     const buffer = Buffer.from(arrayBuffer);
 
-    console.log(`Writing img to disk for ${name}...`);
+    logInfo(`> Writing image to disk ${name}`);
+    // console.log(`Writing img to disk for ${name}...`);
 
     // write image file to disk
     const imageFileName = `${id}.${extension}`;
     await writeFile(`${STATICS_PATH}/${imageFileName}`, buffer);
 
+    logInfo(`> Everything is done! ${name}`);
     return { id, name, image: imageFileName, teamId: 0 };
   })
 );
 
-console.log("All presidents are done!");
+logSuccess("> All presidents are done!");
+// console.log("All presidents are done!");
 await writeFile(
   `${DB_PATH}/presidents.json`,
   JSON.stringify(presidents, null, 2)
