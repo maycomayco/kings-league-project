c083a667bd7166a5178578b7a31ba21ff29f8cb4
Mayco Barale <maycobarale@gmail.com>
2023-01-12 19:26:36 -0300

    add scraping for coaches

--- scraping/coaches.js
+++ scraping/coaches.js
@@ -1,30 +1,29 @@
-export const getCoachs = async (cheerioInput) => {
-  const INFO_COACHS_SELECTORS = {
+export const getCoaches = async (cheerioInput) => {
+  const INFO_COACHES_SELECTORS = {
     teamName: { selector: ".name.mt10", typeOf: "string" },
     coach: { selector: ".name.mt20", typeOf: "string" },
     coachImg: { selector: ".player-circle-box", typeOf: "string" },
   };
 
-  // const $ = await scrape(URLS.coachs);
-  const coachsTeam = cheerioInput(INFO_COACHS_SELECTORS.coach.selector)
+  const coachesTeam = cheerioInput(INFO_COACHES_SELECTORS.coach.selector)
     .toArray()
     .map((coachName) => coachName.children[0].data);
-  const coachsImgTeam = cheerioInput(INFO_COACHS_SELECTORS.coachImg.selector)
+  const coachesImgTeam = cheerioInput(INFO_COACHES_SELECTORS.coachImg.selector)
     .toArray()
     .map((coachImg) => {
       const { attribs } = coachImg;
       const { src } = attribs;
       return src;
     });
-  const teamsName = cheerioInput(INFO_COACHS_SELECTORS.teamName.selector)
+  const teamsName = cheerioInput(INFO_COACHES_SELECTORS.teamName.selector)
     .toArray()
     .map((teamName) => teamName.children[0].data);
-  const teamsWithCoach = coachsTeam.map((coach, i) => {
+  const teamsWithCoach = coachesTeam.map((coach, i) => {
     return {
       name: coach,
       // teamName: replaceFCOfTeamName(teamsName[i]),
       teamName: teamsName[i],
-      image: coachsImgTeam[i],
+      image: coachesImgTeam[i],
     };
   });
   return teamsWithCoach;
