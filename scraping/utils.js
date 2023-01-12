70619c71f1ff8f65ea3d4dd6fa19a02193169623
Mayco Barale <maycobarale@gmail.com>
2023-01-12 11:02:04 -0300

    Add new scraping logic

--- scraping/utils.js
+++ scraping/utils.js
@@ -1,18 +1,58 @@
 import * as cheerio from "cheerio";
+import { writeDBFile } from "../db/index.js";
+import { getLeaderboard } from "./leaderboard.js";
+import { logError, logInfo, logSuccess } from "./logs.js";
+import { getMvp } from "./mvp.js";
 
-export const URLS = {
-  leaderboard: "https://kingsleague.pro/estadisticas/clasificacion/",
-  mvp: "https://kingsleague.pro/estadisticas/mvp/",
+export const SCRAPINGS = {
+  leaderboard: {
+    url: "https://kingsleague.pro/estadisticas/clasificacion/",
+    scraper: getLeaderboard,
+  },
+  mvp: {
+    url: "https://kingsleague.pro/estadisticas/mvp/",
+    scraper: getMvp,
+  },
 };
 
+// method to scrapte a specific url
 export async function scrape(url) {
   const res = await fetch(url);
   const html = await res.text();
   return cheerio.load(html);
 }
 
+// method to clean text from a rows
 export const cleanText = (text) =>
   text
     .replace(/\t|\n|\s:/g, "")
     .replace(/.*:/g, " ")
     .trim();
+
+export const scrapeAndSave = async (name) => {
+  // empezamos a medir el tiempo de ejecución de la función
+  const start = performance.now();
+
+  try {
+    const { scraper, url } = SCRAPINGS[name];
+
+    logInfo(`Scraping [${name}] list...`);
+    const cheerioResult = await scrape(url);
+    // ejecutamos la función scraper por parametro
+    const content = await scraper(cheerioResult);
+    logSuccess(`[${name}] scraped successfully!`);
+
+    logInfo(`Writing [${name}] to DB...`);
+    await writeDBFile(name, content);
+    logSuccess(`[${name}] written to DB successfully!`);
+  } catch (error) {
+    logError(`Error scraping [${name}]!`);
+    logError(error);
+  } finally {
+    // tomamos el tiempo de ejecución de la función hasta aqui
+    const end = performance.now();
+    // calculamos el tiempo de ejecución de la función en segundos (time/1000)
+    const time = (end - start) / 1000;
+    logInfo(`[${name}] scraped in ${time} seconds`);
+  }
+};
