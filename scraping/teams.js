dfea6dc673c6af9ee48e66c601bcf2c0b1edea4e
Mayco Barale <maycobarale@gmail.com>
2023-01-18 09:43:25 -0300

    Revert "add players to DB"
    This reverts commit abebb654323070621ab38d8ca1430c1555266fce.

--- scraping/teams.js
+++ scraping/teams.js
@@ -1,11 +1,11 @@
-import path from 'node:path'
-import sharp from 'sharp'
-
-import { TEAMS, writeDBFile } from '../db/index.js'
+import { TEAMS } from '../db/index.js'
 import { logInfo, logSuccess } from './log.js'
 import { cleanText, scrape } from './utils.js'
+import path from 'node:path'
+import { writeFile } from 'node:fs/promises'
 
-const STATICS_PATH = path.join(process.cwd(), './public/teams/')
+const STATICS_PATH = path.join(process.cwd(), './assets/static')
+const DB_PATH = path.join(process.cwd(), './db/')
 const BASE_URL = 'https://kingsleague.pro/team'
 const SELECTORS = {
 	name: '.el-title',
@@ -13,45 +13,25 @@ const SELECTORS = {
 	image: '.el-image'
 }
 
-const MAPPER_STATS_PLAYER = {
-	reflejo: 'reflexes',
-	paradas: 'saves',
-	saque: 'kickoff',
-	estirada: 'stretch',
-	velocidad: 'speed',
-	físico: 'physique',
-	tiro: 'shooting',
-	pase: 'passing',
-	talento: 'talent',
-	defensa: 'defense'
-}
-
 async function getTeams() {
 	const teams = []
 
 	const saveImage = async ({ url, folder, fileName }) => {
+		const fileExtension = url.split('.').at(-1)
+
 		logInfo(`Fetching image for file name: ${fileName}`)
 		const responseImage = await fetch(url)
 		const arrayBuffer = await responseImage.arrayBuffer()
 		const buffer = Buffer.from(arrayBuffer)
 
 		logInfo(`Writing image to disk ${fileName}`)
-		const imageFileNameClean = removeCharacters(fileName)
-		const imageFileName = `${imageFileNameClean}.webp`
+		const imageFileName = `${fileName}.${fileExtension}`
 		const imageFilePath = path.join(STATICS_PATH, folder, imageFileName)
-		await sharp(buffer).webp().toFile(imageFilePath)
+		await writeFile(imageFilePath, buffer)
 
 		logInfo(`Everything is done! ${fileName}`)
 
 		return imageFileName
-	}
-
-	const extractIdFromUrl = (url) => url.split('/').at(-1).split('.').at(0)
-
-	const removeCharacters = (s) => {
-		const withOutAccents = s.normalize('NFD').replace(/[\u0300-\u036f]/g, '')
-		const regex = /[^a-zA-Z0-9.-]/g
-		return withOutAccents.replace(regex, '')
 	}
 
 	const onlyLettersString = (s) => {
@@ -80,7 +60,7 @@ async function getTeams() {
 			const $el = $(el)
 
 			const nameRawValue = $el.find(SELECTORS.name).text()
-			const dorsalName = cleanText(nameRawValue)
+			const name = cleanText(nameRawValue)
 
 			// .el-content > "" + span
 			// .el-content > p > "" + span
@@ -94,53 +74,22 @@ async function getTeams() {
 
 			if (roleLowerCase !== 'presidente' && roleLowerCase !== 'entrenador') {
 				const url = $el.find(SELECTORS.image).attr('src')
-				const nameKebabCase = convertStringToKebabCase(dorsalName)
+				const nameKebabCase = convertStringToKebabCase(name)
 				const fileName = `${teamId}-${nameKebabCase}`
 				const image = await saveImage({ url, folder: 'players', fileName })
 
-				const dorsal = extractIdFromUrl(url)
-				const playerStatsClass = $el.find('.id-player').text().replace(' ', '-')
-				const $stats = $(`.${playerStatsClass} .data-player, .${playerStatsClass} .data-goalk`)
-				const fullName = cleanText($(`.${playerStatsClass} h1`).text())
-
-				const statsInfo = {}
-				if ($stats.length > 0) {
-					const $statsForPlayer = $stats.find('> div > div')
-
-					$statsForPlayer.each((_, el) => {
-						const $statEl = $(el)
-						const statTitle = cleanText($statEl.find('.el-meta').text()).toLowerCase()
-						const statValue = cleanText($statEl.find('.el-title').text())
-
-						const key = MAPPER_STATS_PLAYER[statTitle]
-						const value = Number(statValue)
-						if (value > 0) statsInfo[key] = value
-					})
-				}
-
-				const availableStats = Object.keys(statsInfo).length
-				const statsScore = availableStats
-					? Object.values(statsInfo).reduce((acc, value) => acc + value) / availableStats
-					: null
-
 				players.push({
-					id: `${teamId}-${dorsal}`,
-					dorsalName,
-					fullName,
+					name,
 					role,
-					image,
-					stats: {
-						...statsInfo,
-						...(statsScore && { score: Math.round(statsScore) })
-					}
+					image
 				})
 			} else if (roleLowerCase === 'entrenador') {
 				const url = $el.find(SELECTORS.image).attr('src')
-				const fileName = convertStringToKebabCase(dorsalName)
+				const fileName = convertStringToKebabCase(name)
 				const image = await saveImage({ url, folder: 'coaches', fileName })
 
 				team.coachInfo = {
-					name: dorsalName,
+					name,
 					image
 				}
 			}
@@ -158,4 +107,4 @@ async function getTeams() {
 }
 
 const teams = await getTeams()
-await writeDBFile('teams', teams)
+await writeFile(`${DB_PATH}/teams.json`, JSON.stringify(teams, null, 2), 'utf-8')
