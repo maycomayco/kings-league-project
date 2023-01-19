85ebdfd8e4fc174980a0a3d72cd364d393c6ad10
Mayco Barale <maycobarale@gmail.com>
2023-01-19 18:50:47 -0300

    replace wrong URL

--- scraping/players_twelve.js
+++ scraping/players_twelve.js
@@ -69,7 +69,7 @@ async function saveImageBase64(player) {
 	const { firstName, lastName, team, image } = player
 
 	if (image.includes('placeholder.png')) {
-		return 'https://api.kingsleague.dev/static/players/placeholder.png'
+		return 'https://kings-league.maycobarale.workers.dev/static/players/placeholder.png'
 	}
 
 	let playerImage = null
@@ -91,7 +91,7 @@ async function saveImageBase64(player) {
 
 		await writeFile(`${PLAYER_FOLDER_PATH}/${normalizedImageName}`, imageBase64, 'base64')
 
-		playerImage = `https://api.kingsleague.dev/static/players/${normalizedImageName}`
+		playerImage = `https://kings-league.maycobarale.workers.dev/static/players/${normalizedImageName}`
 	} catch (error) {
 		console.log(error)
 	}
