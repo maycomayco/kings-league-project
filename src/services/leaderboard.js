4241f235352479e2f923cf5eac2300de82e6d688
Mayco Barale <maycobarale@gmail.com>
2023-01-19 10:07:49 -0300

    add leaderboard service

--- src/services/leaderboard.js
+++ src/services/leaderboard.js
@@ -1,19 +1,21 @@
-import { apiURL } from './config.js'
+// import { apiURL } from './config.js'
 
-export const getLeaderboard = async () => {
-	try {
-		const response = await fetch(`${apiURL}/leaderboard`)
-		const leaderboard = await response.json()
-		return leaderboard
-	} catch (e) {
-		// enviar el error a un servicio de reporte de errores
-		return null
-	}
-}
+// export const getLeaderboard = async () => {
+// 	try {
+// 		const response = await fetch(`${apiURL}/leaderboard`)
+// 		const leaderboard = await response.json()
+// 		return leaderboard
+// 	} catch (e) {
+// 		// enviar el error a un servicio de reporte de errores
+// 		return null
+// 	}
+// }
 
 export const findLeaderboardBy = async ({ teamId }) => {
 	try {
-		const response = await fetch(`${apiURL}/leaderboard/${teamId}`)
+		const response = await fetch(
+			`https://kings-league.maycobarale.workers.dev/leaderboard/${teamId}`
+		)
 		const teamStats = await response.json()
 		return teamStats
 	} catch (e) {
