f8b59afce90a0f54117385011b5d5411f02ebe68
Mayco Barale <maycobarale@gmail.com>
2023-01-24 11:06:02 -0300

    update db files

--- db/index.js
+++ db/index.js
@@ -1,29 +1,23 @@
-import { writeFile, readFile } from "node:fs/promises";
-import path from "node:path";
+import { writeFile, readFile } from 'node:fs/promises'
+import path from 'node:path'
 
 // general DB path
-const DB_PATH = path.join(process.cwd(), "./db/");
+const DB_PATH = path.join(process.cwd(), './db/')
 
 // Read DB file
-const readDBFile = async (dbName) => {
-  return readFile(path.join(DB_PATH, `${dbName}.json`), "utf-8").then(
-    JSON.parse
-  );
-};
+export const readDBFile = async (dbName) => {
+	return readFile(path.join(DB_PATH, `${dbName}.json`), 'utf-8').then(JSON.parse)
+}
 
 export const writeDBFile = async (dbName, data) => {
-  return writeFile(
-    path.join(DB_PATH, `${dbName}.json`),
-    JSON.stringify(data, null, 2),
-    "utf-8"
-  );
-};
+	return writeFile(path.join(DB_PATH, `${dbName}.json`), JSON.stringify(data, null, 2), 'utf-8')
+}
 
 export const getImageFromTeam = ({ name }) => {
-  const { image } = TEAMS.find((team) => team.name === name);
-  return image;
-};
+	const { image } = TEAMS.find((team) => team.name === name)
+	return image
+}
 
 // general files to read in the entire project
-export const TEAMS = await readDBFile("teams");
-export const PRESIDENTS = await readDBFile("presidents");
+export const TEAMS = await readDBFile('teams')
+export const PRESIDENTS = await readDBFile('presidents')
