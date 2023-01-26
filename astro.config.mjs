6a47986573399ca6ca6cc5d4cc2900f7371faccb
Mayco Barale <maycobarale@gmail.com>
2023-01-26 11:57:26 -0300

    add astro critters pkg

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind'
+import critters from 'astro-critters'
+
+// TODO, add prefetch astro plugin
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
-});
+	integrations: [tailwind(), critters()]
+})
